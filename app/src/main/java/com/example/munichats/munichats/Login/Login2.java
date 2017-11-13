37717671770388d059c66e12897442a6dd392943
Hoanganhken <hoanganhken98@gmail.com>
2017-11-13 21:21:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/munichats/munichats/Login/Login2.java
@@ -0,0 +1,15 @@
+package com.example.munichats.munichats.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.munichats.munichats.R;
+
+public class Login2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
---
+++ app/src/main/java/com/example/munichats/munichats/Login/Registration.java
@@ -0,0 +1,15 @@
+package com.example.munichats.munichats.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.munichats.munichats.R;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
